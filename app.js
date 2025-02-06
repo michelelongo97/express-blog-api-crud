64d01cbc38a082718b029a4446b30ba7c35dd18e
michelelongo97 <m.longo7x@gmail.com>
2025-02-06 15:56:33 +0100

    feat: add cors

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
 
 const errorsHandler = require("./middlewares/errorsHandler");
 const notFound = require("./middlewares/notFound");
 
+const postsRouter = require("./routers/posts");
 
-const postsRouter = require('./routers/posts')
+app.use(express.json());
+app.use(express.static("public"));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
-app.use(express.json())
-app.use(express.static('public'))
+app.get("/", (req, res) => {
+  res.send("Server del mio blog");
+});
 
-app.get('/', (req, res) => {
-  res.send('Server del mio blog')
-})
-
-app.use('/posts', postsRouter)
+app.use("/posts", postsRouter);
 
 //middleware per error404
 app.use(notFound);
 app.use(errorsHandler);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
